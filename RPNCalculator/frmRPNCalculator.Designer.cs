72819f3c0fc33f2411b976c2f86ac4452b66756c
iweating <iweating@indiana.edu>
2014-11-19 15:00:49 -0500

    Initial Commit

---
+++ RPNCalculator/frmRPNCalculator.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace RPNCalculator
+{
+    partial class frmRPNCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubt = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.txtFirstNum = new System.Windows.Forms.TextBox();
+            this.txtSecondNum = new System.Windows.Forms.TextBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.lblFirstNumber = new System.Windows.Forms.Label();
+            this.lblSecondNumber = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(133, 98);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(23, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubt
+            // 
+            this.btnSubt.Location = new System.Drawing.Point(162, 98);
+            this.btnSubt.Name = "btnSubt";
+            this.btnSubt.Size = new System.Drawing.Size(23, 23);
+            this.btnSubt.TabIndex = 1;
+            this.btnSubt.Text = "-";
+            this.btnSubt.UseVisualStyleBackColor = true;
+            this.btnSubt.Click += new System.EventHandler(this.btnSubt_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(133, 127);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(23, 23);
+            this.btnMult.TabIndex = 2;
+            this.btnMult.Text = "x";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(162, 127);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(23, 23);
+            this.btnDiv.TabIndex = 3;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(54, 212);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(135, 212);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // txtFirstNum
+            // 
+            this.txtFirstNum.Location = new System.Drawing.Point(110, 46);
+            this.txtFirstNum.Name = "txtFirstNum";
+            this.txtFirstNum.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstNum.TabIndex = 6;
+            this.txtFirstNum.Leave += new System.EventHandler(this.txtFirstNum_Leave);
+            this.txtFirstNum.MouseDown += new System.Windows.Forms.MouseEventHandler(this.txtFirstNum_MouseDown);
+            // 
+            // txtSecondNum
+            // 
+            this.txtSecondNum.Location = new System.Drawing.Point(110, 72);
+            this.txtSecondNum.Name = "txtSecondNum";
+            this.txtSecondNum.Size = new System.Drawing.Size(100, 20);
+            this.txtSecondNum.TabIndex = 7;
+            this.txtSecondNum.Leave += new System.EventHandler(this.txtSecondNum_Leave);
+            this.txtSecondNum.MouseDown += new System.Windows.Forms.MouseEventHandler(this.txtSecondNum_MouseDown);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(110, 165);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 8;
+            // 
+            // lblFirstNumber
+            // 
+            this.lblFirstNumber.AutoSize = true;
+            this.lblFirstNumber.Location = new System.Drawing.Point(39, 49);
+            this.lblFirstNumber.Name = "lblFirstNumber";
+            this.lblFirstNumber.Size = new System.Drawing.Size(69, 13);
+            this.lblFirstNumber.TabIndex = 9;
+            this.lblFirstNumber.Text = "First Number:";
+            // 
+            // lblSecondNumber
+            // 
+            this.lblSecondNumber.AutoSize = true;
+            this.lblSecondNumber.Location = new System.Drawing.Point(21, 75);
+            this.lblSecondNumber.Name = "lblSecondNumber";
+            this.lblSecondNumber.Size = new System.Drawing.Size(87, 13);
+            this.lblSecondNumber.TabIndex = 10;
+            this.lblSecondNumber.Text = "Second Number:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(60, 168);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(48, 13);
+            this.lblAnswer.TabIndex = 11;
+            this.lblAnswer.Text = "Answer: ";
+            // 
+            // frmRPNCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 277);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblSecondNumber);
+            this.Controls.Add(this.lblFirstNumber);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.txtSecondNum);
+            this.Controls.Add(this.txtFirstNum);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnSubt);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "frmRPNCalculator";
+            this.Text = "RPN Calculator";
+            this.Load += new System.EventHandler(this.frmRPNCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubt;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.TextBox txtFirstNum;
+        private System.Windows.Forms.TextBox txtSecondNum;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label lblFirstNumber;
+        private System.Windows.Forms.Label lblSecondNumber;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ RPNCalculator/frmRPNCalculator.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPNCalculator
+{
+    public partial class frmRPNCalculator : Form
+    {
+        double dblFirstNum = 0;
+        double dblSecondNum = 0;
+        double dblAnswer = 0;
+        string strFirstNum = "0";
+        string strSecondNum = "0";
+        string strAnswer = "0";
+        string strTest = "0";
+        double dblNumCheck = 0;
+        public bool isNum;
+
+
+        public frmRPNCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void txtFirstNum_Leave(object sender, EventArgs e)
+        {
+            strFirstNum = txtFirstNum.Text.ToString();
+            strTest = strFirstNum;
+
+            bool isNum = double.TryParse(strTest,System.Globalization.NumberStyles.Number,
+                System.Globalization.CultureInfo.CurrentCulture.NumberFormat,out dblNumCheck);
+
+            if (isNum)
+            {
+                dblFirstNum = double.Parse(strFirstNum);
+            }
+            else
+            {
+                txtFirstNum.Text = "";
+                strFirstNum = "";
+                txtFirstNum.Lines.Initialize();
+                MessageBox.Show("That is not a number. PRESS CLEAR and try again");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstNum.Text = "0";
+            txtSecondNum.Text = "0";
+            txtAnswer.Text = "0";
+        }
+
+        private void txtSecondNum_Leave(object sender, EventArgs e)
+        {
+            strSecondNum = txtSecondNum.Text.ToString();
+            strTest = strSecondNum;
+
+            bool isNum = double.TryParse(strTest, System.Globalization.NumberStyles.Number,
+                System.Globalization.CultureInfo.CurrentCulture.NumberFormat, out dblNumCheck);
+
+            if (isNum)
+            {
+                dblSecondNum = double.Parse(strSecondNum);
+            }
+            else
+            {
+                txtSecondNum.Text = "";
+                strSecondNum = "";
+                txtSecondNum.Lines.Initialize();
+                MessageBox.Show("That is not a number. PRESS CLEAR and try again");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            dblAnswer = (dblFirstNum + dblSecondNum);
+            strAnswer = dblAnswer.ToString();
+            txtAnswer.Text = strAnswer;
+        }
+
+        private void btnSubt_Click(object sender, EventArgs e)
+        {
+            dblAnswer = (dblFirstNum - dblSecondNum);
+            strAnswer = dblAnswer.ToString();
+            txtAnswer.Text = strAnswer;
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            dblAnswer = (dblFirstNum * dblSecondNum);
+            strAnswer = dblAnswer.ToString();
+            txtAnswer.Text = strAnswer;
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            dblAnswer = (dblFirstNum / dblSecondNum);
+            strAnswer = dblAnswer.ToString();
+            txtAnswer.Text = strAnswer;
+        }
+
+        private void frmRPNCalculator_Load(object sender, EventArgs e)
+        {
+            txtFirstNum.Text = "First Number";
+            txtSecondNum.Text = "Second Number";
+        }
+
+        private void txtFirstNum_MouseDown(object sender, MouseEventArgs e)
+        {
+            txtFirstNum.Text = "";
+        }
+
+        private void txtSecondNum_MouseDown(object sender, MouseEventArgs e)
+        {
+            txtSecondNum.Text = "";
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
